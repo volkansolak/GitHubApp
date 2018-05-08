a1a1632beeaab6877ddb37f082702e2e96017af1
Volkan Solak <nvolkansolak@gmail.com>
2018-05-08 21:18:41 +0300

    Initial Commit

---
+++ GitHubApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubApp
+//
+//  Created by VolkanSolak on 8.05.2018.
+//  Copyright © 2018 VolkanSolak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
